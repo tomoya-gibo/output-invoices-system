6935df616d57a8be2eb44be57a4fb47045c66ef3
Yumeka Ota <ota@sabira.jp>
2025-10-14 11:29:11 +0900

    .jsonのインポート確認

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-function main() {
-  console.log("Hello, world!");
+const invoices = require('../input/invoices.json');
+const plays = require('../input/plays.json');
+
+function main() {	
+	let company = invoices[0].customer;
+	let playTitle = invoices[0].performances[1].playID;
+	console.log(`${company} : ${playTitle}`);
+	console.log(plays['as-like']);
 }
 
 main();
