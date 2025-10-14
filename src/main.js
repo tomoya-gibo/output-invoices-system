1aa0b652f2da0d518dfa764e6cec502ee4e152dd
Yumeka Ota <ota@sabira.jp>
2025-10-14 13:24:16 +0900

    変数を作成

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
+//jsonファイルのインポート
 const invoices = require('../input/invoices.json');
 const plays = require('../input/plays.json');
 
-function main() {	
-	let company = invoices[0].customer;
-	let playTitle = invoices[0].performances[1].playID;
-	console.log(`${company} : ${playTitle}`);
-	console.log(plays['as-like']);
+function main() {
+	//使いそうな変数　△let/const
+	const company = invoices[0].customer;			//会社名
+	let performances = invoices[0].performances;	//演目(dic)
+	let point;				//ポイント
+	let amount;				//金額
+	let totalAmount;		//合計金額
+	let resultData;			//請求内容
+	
+	
+	
 }
 
 main();
