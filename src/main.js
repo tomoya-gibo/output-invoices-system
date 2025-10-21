dc7896699ee4bc1f37ae4933187c0b84c2c21691
Yumeka Ota <ota@sabira.jp>
2025-10-21 13:43:40 +0900

    確認用を追加

--- src/main.js
+++ src/main.js
@@ -48,11 +48,13 @@ function main() {
 
 	// ファイルへ書き込む
 	if (args === "txt") {
+		console.log(args);
 		fs.writeFileSync("../output/invoice.txt", resultTxt);
 	} else if (args === "html") {
+		console.log(args);
 		fs.writeFileSync("../output/invoice.html", resultHtml);
 	} else {
-		console.log("txt/html以外の入力");
+		console.log("args:" + args);
 	}
 }
 
