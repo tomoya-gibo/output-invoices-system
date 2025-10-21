c4f7c777393f9c9059c18dad00f93a7a0e678dda
kouhei <iju@sabira.jp>
2025-10-21 09:43:43 +0900

    追加 : ジャンルが悲喜劇の処理を実装

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@
               thisAmount += (performance.audience - 30) * 1000;
             }
             if (performance.audience > 30){
-              thisPoint += (performance.audience - 30)
+              thisPoint += (performance.audience - 30);
             }
             break;
           case "comedy":
@@ -61,15 +61,21 @@
               thisAmount += (performance.audience - 20) * 500;
             }
             if (performance.audience > 30){
-              thisPoint += (performance.audience - 30)
+              thisPoint += (performance.audience - 30);
             }
             //喜劇の場合のみ超過にかかわらず一人につき$300の追加
             thisAmount += performance.audience * 300;
             //観客数5人につき1ポイント追加
-            thisPoint += Math.floor(performance.audience / 5)
+            thisPoint += Math.floor(performance.audience / 5);
             break;
             case "tragic-comedy":
               thisAmount = tragicomedyBasePrice;
+              //超過にかかわらず観客数一人につき$500追加
+              thisAmount += performance.audience * 500;
+              if (performance.audience > 20) {
+                //20人を超過したら一人につき1ポイント追加。
+                thisPoint += performance.audience - 20;
+              }
             break;
         }
 
