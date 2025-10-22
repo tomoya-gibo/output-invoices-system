c5712282c8cc68349fd99b5683ea1bfc3ec4d030
Yumeka Ota <ota@sabira.jp>
2025-10-22 14:19:19 +0900

    1回目のポイント計算修正、確認

--- src/main.js
+++ src/main.js
@@ -40,17 +40,17 @@ function main() {
 		exTotalAmount += amount;
 	}	
 
-	// //前回(受注1回目)分のポイント計算
-	// for (const performance of performances) {
-	// 	if(plays[performance.playID].type === "tragic-comedy") {	//喜悲劇の場合
-	// 		point += (performance.audience - 20) * 1;
-	// 	} else if (performance.audience > 30) {						//悲劇・喜劇の両方
-	// 		point += (performance.audience - 30) * 1;
-	// 		if (plays[performance.playID].type === "comedy") {		//喜劇の場合加点あり
-	// 		point += Math.floor(performance.audience / 5) * 1;
-	// 		}
-	// 	}
-	// }
+	//前回(受注1回目)分のポイント計算
+	for (const performance of performances) {
+		if(plays[performance.playID].type === "tragic-comedy") {	//喜悲劇の場合
+			exPoint += (performance.audience - 20) * 1;
+		} else if (performance.audience > 30) {						//悲劇・喜劇の両方
+			exPoint += (performance.audience - 30) * 1;
+			if (plays[performance.playID].type === "comedy") {		//喜劇の場合加点あり
+			exPoint += Math.floor(performance.audience / 5) * 1;
+			}
+		}
+	}
 
 	//今回(受注2回目)分の金額計算
 	for (const performance of performances2) {
@@ -97,6 +97,9 @@ function main() {
 	// resultHtml += `</ul><p>合計金額：$${totalAmount}</p><p>獲得ポイント：${point}pt</p>`;
 
 	console.log(exTotalAmount);
+	console.log(exPoint);
+	console.log(totalAmount);
+	console.log(point);
 
 	// // ファイルへ書き込む
 	// console.log("arg:" + arg);
