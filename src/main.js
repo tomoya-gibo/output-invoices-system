6354f637e88bf4cc77ff3a29b179cca3f5d3f7b0
Yumeka Ota <ota@sabira.jp>
2025-10-22 14:14:54 +0900

    1回目の金額計算修正、確認

--- src/main.js
+++ src/main.js
@@ -37,22 +37,20 @@ function main() {
 			amount += 30000;
 			amount += performance.audience * 500;
 		}
-		totalAmount += amount;
-		resultData += `・${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${amount})\n`;
-		resultHtml += `<li>${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${amount})</li><br>`;
+		exTotalAmount += amount;
 	}	
 
-	//前回(受注1回目)分のポイント計算
-	for (const performance of performances) {
-		if(plays[performance.playID].type === "tragic-comedy") {	//喜悲劇の場合
-			point += (performance.audience - 20) * 1;
-		} else if (performance.audience > 30) {						//悲劇・喜劇の両方
-			point += (performance.audience - 30) * 1;
-			if (plays[performance.playID].type === "comedy") {		//喜劇の場合加点あり
-			point += Math.floor(performance.audience / 5) * 1;
-			}
-		}
-	}
+	// //前回(受注1回目)分のポイント計算
+	// for (const performance of performances) {
+	// 	if(plays[performance.playID].type === "tragic-comedy") {	//喜悲劇の場合
+	// 		point += (performance.audience - 20) * 1;
+	// 	} else if (performance.audience > 30) {						//悲劇・喜劇の両方
+	// 		point += (performance.audience - 30) * 1;
+	// 		if (plays[performance.playID].type === "comedy") {		//喜劇の場合加点あり
+	// 		point += Math.floor(performance.audience / 5) * 1;
+	// 		}
+	// 	}
+	// }
 
 	//今回(受注2回目)分の金額計算
 	for (const performance of performances2) {
@@ -98,7 +96,7 @@ function main() {
     // resultData += `獲得ポイント：${point}p（前回比：${diffAmount}pt）\n`;
 	// resultHtml += `</ul><p>合計金額：$${totalAmount}</p><p>獲得ポイント：${point}pt</p>`;
 
-	console.log(resultData);
+	console.log(exTotalAmount);
 
 	// // ファイルへ書き込む
 	// console.log("arg:" + arg);
