04e81e89f95353a6622b082989dab2b51cc2951c
kouhei <iju@sabira.jp>
2025-10-23 10:48:47 +0900

    修正 : 超過人数を割り出す際に、少数が発生するのは不都合なのでMath.floorで切り捨て

--- src/main.js
+++ src/main.js
@@ -64,7 +64,7 @@
               thisAmount += (performance.audience - 20) * 10000;
               console.log(`追加料金:${thisAmount}`);
               //超過した人数はHamletで35人そこから５人超過ごとになので35/5で７回分の割引が発生
-              excess = performance.audience - 20 / 5;
+              excess = Math.floor((performance.audience - 20) / 5);
               console.log(`超過人数:${excess}`);
               thisAmount = Math.floor(thisAmount * (excess * 0.1));
               console.log(`５人超過ごとの割り引いた金額:${thisAmount}`);
