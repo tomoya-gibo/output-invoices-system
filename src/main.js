7e3930255f8ec5c39f286016bf45dc9ca20e5023
kouhei <iju@sabira.jp>
2025-10-23 10:51:59 +0900

    修正 : 処理の実装が完了したので確認用のログをコメントアウト

--- src/main.js
+++ src/main.js
@@ -62,12 +62,12 @@
               //金額の小数は切捨て
               //↓が超過の料金
               thisAmount += (performance.audience - 20) * 10000;
-              console.log(`追加料金:${thisAmount}`);
+              //console.log(`追加料金:${thisAmount}`);
               //超過した人数はHamletで35人そこから５人超過ごとになので35/5で７回分の割引が発生
               excess = Math.floor((performance.audience - 20) / 5);
-              console.log(`超過人数:${excess}`);
+              //console.log(`超過人数:${excess}`);
               thisAmount = Math.floor(thisAmount * (excess * 0.1));
-              console.log(`５人超過ごとの割り引いた金額:${thisAmount}`);
+              //console.log(`５人超過ごとの割り引いた金額:${thisAmount}`);
             }
             if (performance.audience > 30) {
               thisPoint += (performance.audience - 30);
