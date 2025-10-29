5bf8eab1af2c1cf2461891bfb8869af013968a99
Yumeka Ota <ota@sabira.jp>
2025-10-29 10:32:57 +0900

    関数宣言を移動、自分用にコメント整理

--- src/main.js
+++ src/main.js
@@ -3,36 +3,19 @@ export function main() {
   const invoices = JSON.parse(fs.readFileSync("input/invoices.json", "utf8"));
   const plays = JSON.parse(fs.readFileSync("input/plays.json", "utf8"));
 
-	let point = 0;				//ポイント
+	let point = 0;
 		
 	//金額計算
-	const performances = invoices[0].performances;	//演目(dic)
-	let amount = 0;				//金額
-	let totalAmount = 0;		//合計金額
+	const performances = invoices[0].performances;	//演目
+	let amount = 0;									//金額
+	let totalAmount = 0;							//合計金額
 	for (const performance of performances) {
 		amount = calculateAmount(plays, performance);
 		totalAmount += amount;
 	}
-
-	function calculateAmount(plays, performance) {
-		amount = 0;
-		if (plays[performance.playID].type === "tragedy") {		//悲劇の場合
-			amount += 40000;
-			if (performance.audience > 30) {	//観客数の超過料金計算
-				amount += (performance.audience - 30) * 1000;
-			}
-		} else {	//喜劇の場合
-			amount += 30000;
-			amount += performance.audience * 300;
-			if (performance.audience > 20) {
-				amount += 10000;
-				amount += (performance.audience - 20) * 500;
-			}
-		}
-		return amount;
-	}
 	
-	let resultData = `請求書\n\n${invoices[0].customer}\n\n`;		//請求内容出力用
+	//請求内容作成
+	let resultData = `請求書\n\n${invoices[0].customer}\n\n`;
 	for (const performance of performances) {
 		amount = calculateAmount(plays, performance);
 		resultData += `・${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${amount})\n`;
@@ -45,6 +28,25 @@ export function main() {
 
 	printTxt(resultData);
 
+
+
+	function calculateAmount(plays, performance) {
+		amount = 0;
+		if (plays[performance.playID].type === "tragedy") {		//悲劇の場合
+			amount += 40000;
+			if (performance.audience > 30) {					//観客数の超過料金計算
+				amount += (performance.audience - 30) * 1000;
+			}
+		} else {												//喜劇の場合
+			amount += 30000;
+			amount += performance.audience * 300;
+			if (performance.audience > 20) {
+				amount += 10000;
+				amount += (performance.audience - 20) * 500;
+			}
+		}
+		return amount;
+	}
 
 	function calculatePoint() {
 		for (const performance of performances) {
