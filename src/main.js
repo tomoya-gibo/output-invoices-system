6a07f3c693da7e6e26842e26f1ad81771afcfe62
Yumeka Ota <ota@sabira.jp>
2025-10-30 10:40:12 +0900

    関数宣言の変更：呼び出し箇所を変更

--- src/main.js
+++ src/main.js
@@ -70,7 +70,7 @@ export function main() {
 			invoiceTxt += `・${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${amount(plays, performance)})\n`;
 		}
 		invoiceTxt += `\n合計金額：$${calcTotalAmount(performances)}\n\n`;
-		invoiceTxt += `獲得ポイント：${point()}pt\n`;
+		invoiceTxt += `獲得ポイント：${calcTotalPoint(plays, performances)}pt\n`;
 		return invoiceTxt;
 	}
 
