d45287bf34ef09be0db15f99c706593181072f96
Yumeka Ota <ota@sabira.jp>
2025-11-04 15:56:15 +0900

    引数を追加

--- tests/main/function/render.test.js
+++ tests/main/function/render.test.js
@@ -11,7 +11,8 @@ describe('renderTxtのテスト', () => {
         const performances = [{ "playID" : "hamlet", "audience" : 55 },
                               { "playID" : "as-like", "audience" : 35 },
                               { "playID" : "othello", "audience" : 40 }];
-        const testData = renderTxt(invoices, plays, performances);
+        const arg = "txt";
+        const testData = renderTxt(invoices, plays, performances, arg);
         const expectData = `請求書\n\nCustomer\n\n・Hamlet (観客数:55人、金額:$65000)\n・As You Like It (観客数:35人、金額:$58000)\n`
             + `・Othello (観客数:40人、金額:$50000)\n\n合計金額：$173000\n\n獲得ポイント：47pt\n`;
         expect(testData).toBe(expectData);
