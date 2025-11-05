b0e6542f8cf844d8e3441c3584fcdff9a227bdf5
Yumeka Ota <ota@sabira.jp>
2025-11-05 15:18:37 +0900

    不要なテストとインポートを削除

--- tests/main/function/render.test.js
+++ tests/main/function/render.test.js
@@ -1,38 +1,9 @@
 import { describe, test, expect } from "vitest";
-import { renderInvoiceHtml,
-         renderInvoiceTxt,
-         renderTxt } from '../../../src/main.js';
+import { renderInvoiceHtml, renderInvoiceTxt } from '../../../src/main.js';
 
 const plays = { "hamlet": { "name": "Hamlet", "type": "tragedy" },
                 "as-like" : { "name": "As You Like It", "type": "comedy" },
                 "othello" : { "name": "Othello", "type": "tragedy" } };
-
-describe('renderTxtのテスト', () => {
-    test('testCase1, txt', () => {
-        const invoices = [{ "customer" : "Customer"}];
-        const performances = [{ "playID" : "hamlet", "audience" : 55 },
-                              { "playID" : "as-like", "audience" : 35 },
-                              { "playID" : "othello", "audience" : 40 }];
-        const arg = "txt";
-        const testData = renderTxt(invoices, plays, performances, arg);
-        const expectData = `請求書\n\nCustomer\n\n・Hamlet (観客数:55人、金額:$65000)\n・As You Like It (観客数:35人、金額:$58000)\n`
-            + `・Othello (観客数:40人、金額:$50000)\n\n合計金額：$173000\n\n獲得ポイント：47pt\n`;
-        expect(testData).toBe(expectData);
-    })
-
-    test('testCase2, html', () => {
-        const invoices = [{ "customer" : "Customer"}];
-        const performances = [{ "playID" : "hamlet", "audience" : 55 },
-                              { "playID" : "as-like", "audience" : 35 },
-                              { "playID" : "othello", "audience" : 40 }];
-        const arg = "html";
-        const testData = renderTxt(invoices, plays, performances, arg);
-        const expectData = `<h3>請求書</h3><h4>Customer</h4><ul><li>Hamlet (観客数:55人、金額:$65000)</li><br>`
-            + `<li>As You Like It (観客数:35人、金額:$58000)</li><br><li>Othello (観客数:40人、金額:$50000)</li><br>`
-            + `</ul><p>合計金額：$173000</p><p>獲得ポイント：47pt</p>`;
-        expect(testData).toBe(expectData);
-    })
-})
 
 describe('renderInvoiceTxtのテスト', () => {
     test('testCase1', () => {
