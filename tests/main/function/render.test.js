8e70d51e353623f70d1c03277b6ba9b84a3c9a62
Yumeka Ota <ota@sabira.jp>
2025-11-05 10:39:34 +0900

    renderInvoiceTxtのテストを追加

--- tests/main/function/render.test.js
+++ tests/main/function/render.test.js
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import { renderTxt } from '../../../src/main.js';
+import { renderInvoiceTxt, renderTxt } from '../../../src/main.js';
 
 const plays = { "hamlet": { "name": "Hamlet", "type": "tragedy" },
                 "as-like" : { "name": "As You Like It", "type": "comedy" },
@@ -28,6 +28,19 @@ describe('renderTxtのテスト', () => {
         const expectData = `<h3>請求書</h3><h4>Customer</h4><ul><li>Hamlet (観客数:55人、金額:$65000)</li><br>`
             + `<li>As You Like It (観客数:35人、金額:$58000)</li><br><li>Othello (観客数:40人、金額:$50000)</li><br>`
             + `</ul><p>合計金額：$173000</p><p>獲得ポイント：47pt</p>`;
+        expect(testData).toBe(expectData);
+    })
+})
+
+describe('renderInvoiceTxtのテスト', () => {
+    test('testCase1', () => {
+        const invoices = [{ "customer" : "Customer"}];
+        const performances = [{ "playID" : "hamlet", "audience" : 55 },
+                              { "playID" : "as-like", "audience" : 35 },
+                              { "playID" : "othello", "audience" : 40 }];
+        const testData = renderInvoiceTxt(invoices, plays, performances);
+        const expectData = `請求書\n\nCustomer\n\n・Hamlet (観客数:55人、金額:$65000)\n・As You Like It (観客数:35人、金額:$58000)\n`
+            + `・Othello (観客数:40人、金額:$50000)\n\n合計金額：$173000\n\n獲得ポイント：47pt\n`;
         expect(testData).toBe(expectData);
     })
 })
