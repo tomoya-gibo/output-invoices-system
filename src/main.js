68ecdf3e8c4307ff2cb9143d7b84e62b5092c3dc
Yumeka Ota <ota@sabira.jp>
2025-11-05 10:23:34 +0900

    関数の抽出：元の関数をコピー

--- src/main.js
+++ src/main.js
@@ -75,9 +75,14 @@ export function renderInvoiceTxt(invoices, plays, performances) {
 }
 
 export function renderInvoiceHtml() {
-	
+	let invoiceHtml = `<h3>請求書</h3><h4>${invoices[0].customer}</h4><ul>`;
+	for (const performance of performances) {
+		invoiceHtml += `<li>${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${calcAmount(plays, performance)})</li><br>`;
+	}
+	invoiceHtml += `</ul><p>合計金額：$${calcTotalAmount(plays, performances)}</p>`;
+	invoiceHtml += `<p>獲得ポイント：${calcTotalPoint(plays, performances)}pt</p>`;
+	return invoiceHtml;
 }
-
 
 export function print(data, arg) {
 	switch(arg) {
