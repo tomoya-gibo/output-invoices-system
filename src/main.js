d09c26fe196c2a83c54f880513ce675432ad9b99
kouhei <iju@sabira.jp>
2025-11-06 15:08:24 +0900

    デッドコードの削除

--- src/main.js
+++ src/main.js
@@ -49,19 +49,16 @@
 
     function calcAmount(play,performance) {
       let thisAmount = 0;
-      let thisPoint = 0;
 
       //演目の種別ごとの料金算定
       switch (play.type) {
         case "tragedy":
           const tragedyBasePrice = 40000;
           thisAmount = tragedyCalc(tragedyBasePrice,performance);
-          thisPoint = pointCalc(performance,play);
           break;
         case "comedy":
           const comedyBasePrice = 30000;
           thisAmount = comedyCalc(comedyBasePrice,performance);
-          thisPoint = pointCalc(performance,play);
           break;
       }
       return thisAmount;
