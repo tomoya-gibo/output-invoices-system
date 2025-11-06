22bfd42bce404c5079312faee7f194f5a7e1cd44
kouhei <iju@sabira.jp>
2025-11-06 13:44:37 +0900

    変数の導入

--- src/main.js
+++ src/main.js
@@ -44,11 +44,12 @@
           thisPoint += pointCalc(performance,play);
           break;
       }
+      let result = {thisAmount,thisPoint};
       //合計金額
-      totalAmount += thisAmount;
+      totalAmount += result.thisAmount;
       //獲得ポイントの合計
-      totalPoint += thisPoint;
-      outputTxt += `・${play.name} (観客数: ${performance.audience}、金額: $${thisAmount})\n`;
+      totalPoint += result.thisPoint;
+      outputTxt += `・${play.name} (観客数: ${performance.audience}、金額: $${result.thisAmount})\n`;
       }
     outputTxt += `\n 合計金額: $${totalAmount}\n 獲得ポイント: ${totalPoint}pt`
     console.log(outputTxt)
