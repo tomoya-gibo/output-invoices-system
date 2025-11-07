b483a50c9787d3ef3c53486dbbd2d5c4eead47e6
Yumeka Ota <ota@sabira.jp>
2025-11-07 13:07:09 +0900

    条件記述の分解：引数を修正

--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@ function isTragedy(plays, performance) {
 	return plays[performance.playID].type === "tragedy";
 }
 
-function calcAmountTragedy() {
+function calcAmountTragedy(audience) {
 	let result = 40000;
-	if (performance.audience > 30) {
-		result += (performance.audience - 30) * 1000;
+	if (audience > 30) {
+		result += (audience - 30) * 1000;
 	}
 	return result;
 }
