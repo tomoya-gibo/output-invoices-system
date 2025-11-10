0c7682b1f9e1c700c17ac244d725f8f4da0878fe
kouhei <iju@sabira.jp>
2025-11-10 15:20:00 +0900

    デッドコードの削除

--- src/main.js
+++ src/main.js
@@ -26,15 +26,6 @@
       totalAmount += amount;
     }
     
-    let totalPoint = 0;
-    for (let performance of invoices[0].performances) {
-      //console.log(performance);
-      // playsのキーとperformance.playIDを照合してplayに代入
-      const play = plays[performance.playID];      
-      let point = pointCalc(performance,play);
-      //獲得ポイントの合計
-      totalPoint += point;
-    }
     let newPoint = calculateTotalPoints(invoices,plays);
     
     // 出力用変数
