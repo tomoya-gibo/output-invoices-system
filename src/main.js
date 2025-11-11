bab6805d85c5adf7bb47b726879817f180be4a0c
kouhei <iju@sabira.jp>
2025-11-11 14:39:05 +0900

    関数の移動 : 関数を移動先にコピー

--- src/main.js
+++ src/main.js
@@ -33,6 +33,18 @@
     
   }
 
+  function buildText(invoices,plays) {
+    let outputTxt = `請求書\n${invoices[0].customer}\n\n`;
+
+    for (let performance of invoices[0].performances) {
+      //console.log(performance);
+      // playsのキーとperformance.playIDを照合してplayに代入
+      const play = plays[performance.playID];
+      outputTxt += `・${play.name} (観客数: ${performance.audience}、金額: $${calcAmount(play,performance)})\n`;
+    }
+    return outputTxt += `\n 合計金額: $${calculateTotalAmounts(invoices,plays)}\n 獲得ポイント: ${calculateTotalPoints(invoices,plays)}pt`
+  }
+
   function calculateTotalAmounts(invoices,plays) {
     let totalAmount = 0;
     for (let performance of invoices[0].performances) {
