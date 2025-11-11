5f27b6a1123c96bdc572bd48303257b2bd76d56c
kouhei <iju@sabira.jp>
2025-11-11 10:19:22 +0900

    ステートメントの移動

--- src/main.js
+++ src/main.js
@@ -19,14 +19,14 @@
     console.log(calculateTotalAmounts(invoices, plays));
     let resultPoint = calculateTotalPoints(invoices,plays);
     
+    function temporaryAmount() {
+
+    }
     
     // 出力用変数
     let outputTxt = buildText(invoices,plays,resultAmount,resultPoint);
     outputFile(outputTxt);
 
-    function temporaryAmount() {
-
-    }
 
     function buildText(invoices,plays,resultAmount,resultPoint) {
       let outputTxt = `請求書\n${invoices[0].customer}\n\n`;
