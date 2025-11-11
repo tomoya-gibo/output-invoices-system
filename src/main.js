43195d19ed57c02ad30c94ac60cc60acc113672b
kouhei <iju@sabira.jp>
2025-11-11 09:43:56 +0900

    デッドコードの削除

--- src/main.js
+++ src/main.js
@@ -16,7 +16,6 @@
     const plays = JSON.parse(fs.readFileSync("input/plays.json", "utf8"));
   
     let resultAmount = calculateTotalAmounts(invoices,plays);
-    let TotalPoint = calculateTotalPoints(invoices,plays);
     let resultPoint = calculateTotalPoints(invoices,plays);
     
     // 出力用変数
