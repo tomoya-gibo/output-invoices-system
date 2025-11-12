6fa8daf700a053ba730863638ab41486b947e9f8
Yumeka Ota <ota@sabira.jp>
2025-11-12 15:49:32 +0900

    ポリモーフィズム：呼び出し側を修正

--- src/main.js
+++ src/main.js
@@ -74,8 +74,7 @@ function isComedy(plays, playID) {
 export function calcTotalAmount(plays, performances) {
 	let totalAmount = 0;
 	for (const performance of performances) {
-		const calc = new Calculator(plays, performance);
-		totalAmount += calc.amount();
+		totalAmount += amount(plays, performance);
 	}
 	return totalAmount;
 }
@@ -83,8 +82,7 @@ export function calcTotalAmount(plays, performances) {
 export function calcTotalPoint(plays, performances) {
 	let result = 0;
 	for (const performance of performances) {
-		const calc = new Calculator(plays, performance);
-		result += calc.point();
+		result += point(plays, performance);
 	}
 	return result;
 }
@@ -93,7 +91,7 @@ export function renderInvoiceTxt(invoice, plays) {
 	let invoiceTxt = `請求書\n\n${invoice.customer}\n\n`;
 	for (const performance of invoice.performances) {
 		const calc = new Calculator(plays, performance);
-		invoiceTxt += `・${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${calc.amount()})\n`;
+		invoiceTxt += `・${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${amount(plays, performance)})\n`;
 	}
 	invoiceTxt += `\n合計金額：$${calcTotalAmount(plays, invoice.performances)}\n\n`;
 	invoiceTxt += `獲得ポイント：${calcTotalPoint(plays, invoice.performances)}pt\n`;
@@ -104,7 +102,7 @@ export function renderInvoiceHtml(invoice, plays) {
 	let invoiceHtml = `<h3>請求書</h3><h4>${invoice.customer}</h4><ul>`;
 	for (const performance of invoice.performances) {
 		const calc = new Calculator(plays, performance);
-		invoiceHtml += `<li>${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${calc.amount()})</li><br>`;
+		invoiceHtml += `<li>${plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${amount(plays, performance)})</li><br>`;
 	}
 	invoiceHtml += `</ul><p>合計金額：$${calcTotalAmount(plays, invoice.performances)}</p>`;
 	invoiceHtml += `<p>獲得ポイント：${calcTotalPoint(plays, invoice.performances)}pt</p>`;
