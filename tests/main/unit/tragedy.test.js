9c88174040650c3b117e2adb1c41690938e21119
Yumeka Ota <ota@sabira.jp>
2025-11-13 15:25:58 +0900

    テスト作成：Tragedy-point()

--- tests/main/unit/tragedy.test.js
+++ tests/main/unit/tragedy.test.js
@@ -15,23 +15,53 @@ describe('Tragedy-amountのテスト', () => {
     })
 
     test('testCase2: 人数超過なし', () => {
-        const performance = { "audience" : 30 };
+        const performance = { "audience": 30 };
         const tragedy = new Tragedy(plays, performance);
         const result = tragedy.amount();
         expect(result).toBe(40000);
     })
 
     test('testCase3: 人数超過あり', () => {
-        const performance = { "audience" : 31 };
+        const performance = { "audience": 31 };
         const tragedy = new Tragedy(plays, performance);
         const result = tragedy.amount();
         expect(result).toBe(41000);
     })
 
     test('testCase4: 人数０', () => {
-        const performance = { "audience" : 0 };
+        const performance = { "audience": 0 };
         const tragedy = new Tragedy(plays, performance);
         const result = tragedy.amount();
         expect(result).toBe(40000);
+    })
+})
+
+describe('Tragedy-pointのテスト', () => {
+    test('testCase1: 人数超過なし', () => {
+        const performance = { "audience": 29 };
+        const tragedy = new Tragedy(plays, performance);
+        const result = tragedy.point();
+        expect(result).toBe(0);
+    })
+
+    test('testCase2: 人数超過なし', () => {
+        const performance = { "audience": 30 };
+        const tragedy = new Tragedy(plays, performance);
+        const result = tragedy.point();
+        expect(result).toBe(0);
+    })
+
+    test('testCase3: 人数超過あり', () => {
+        const performance = { "audience": 31 };
+        const tragedy = new Tragedy(plays, performance);
+        const result = tragedy.point();
+        expect(result).toBe(1);
+    })
+
+    test('testCase4: 人数０', () => {
+        const performance = { "audience": 0 };
+        const tragedy = new Tragedy(plays, performance);
+        const result = tragedy.point();
+        expect(result).toBe(0);
     })
 })
