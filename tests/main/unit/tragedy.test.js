2aa20fa5eb6af53b187c6afaded9304184872742
Yumeka Ota <ota@sabira.jp>
2025-11-13 15:16:42 +0900

    テストを修正：不要な引数を削除

--- tests/main/unit/tragedy.test.js
+++ tests/main/unit/tragedy.test.js
@@ -10,28 +10,28 @@ describe('Tragedy-amountのテスト', () => {
     test('testCase1: 人数超過なし', () => {
         const performance = { "audience": 29 };
         const tragedy = new Tragedy(plays, performance);
-        const result = tragedy.amount(plays, performance);
+        const result = tragedy.amount();
         expect(result).toBe(40000);
     })
 
     test('testCase2: 人数超過なし', () => {
         const performance = { "audience" : 30 };
         const tragedy = new Tragedy(plays, performance);
-        const result = tragedy.amount(plays, performance);
+        const result = tragedy.amount();
         expect(result).toBe(40000);
     })
 
     test('testCase3: 人数超過あり', () => {
         const performance = { "audience" : 31 };
         const tragedy = new Tragedy(plays, performance);
-        const result = tragedy.amount(plays, performance);
+        const result = tragedy.amount();
         expect(result).toBe(41000);
     })
 
     test('testCase4: 人数０', () => {
         const performance = { "audience" : 0 };
         const tragedy = new Tragedy(plays, performance);
-        const result = tragedy.amount(plays, performance);
+        const result = tragedy.amount();
         expect(result).toBe(40000);
     })
 })
