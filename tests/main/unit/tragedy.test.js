48e7097971c857e2f907b57e0880af80f0f0694f
Yumeka Ota <ota@sabira.jp>
2025-11-13 15:13:35 +0900

    テスト作成：Tragedy-amount()

---
+++ tests/main/unit/tragedy.test.js
@@ -0,0 +1,37 @@
+import { describe, test, expect } from "vitest";
+import { Tragedy } from "../../../src/main";
+
+const plays = { "hamlet": { "type": "tragedy" },
+                "as-like": { "type": "comedy" },
+                "othello": { "type": "tragedy" },
+                "romeo-and-juliet": {"type": "tragic-comedy"}};
+
+describe('Tragedy-amountのテスト', () => {
+    test('testCase1: 人数超過なし', () => {
+        const performance = { "audience": 29 };
+        const tragedy = new Tragedy(plays, performance);
+        const result = tragedy.amount(plays, performance);
+        expect(result).toBe(40000);
+    })
+
+    test('testCase2: 人数超過なし', () => {
+        const performance = { "audience" : 30 };
+        const tragedy = new Tragedy(plays, performance);
+        const result = tragedy.amount(plays, performance);
+        expect(result).toBe(40000);
+    })
+
+    test('testCase3: 人数超過あり', () => {
+        const performance = { "audience" : 31 };
+        const tragedy = new Tragedy(plays, performance);
+        const result = tragedy.amount(plays, performance);
+        expect(result).toBe(41000);
+    })
+
+    test('testCase4: 人数０', () => {
+        const performance = { "audience" : 0 };
+        const tragedy = new Tragedy(plays, performance);
+        const result = tragedy.amount(plays, performance);
+        expect(result).toBe(40000);
+    })
+})
