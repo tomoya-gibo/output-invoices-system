14bab8d53d7e74a4aedd88fd66c0b63bd13ad0ba
Yumeka Ota <ota@sabira.jp>
2025-11-13 15:51:49 +0900

    テスト作成：Comedy-point()

--- tests/main/unit/comedy.test.js
+++ tests/main/unit/comedy.test.js
@@ -35,3 +35,61 @@ describe('Comedy-amountのテスト', () => {
         expect(result).toBe(30000);
     })
 })
+
+describe('Comedy-pointのテスト', () => {
+    test('testCase1: 人数超過なし', () => {
+        const performance = { "audience": 4 };
+        const comedy = new Comedy(plays, performance);
+        const result = comedy.point();
+        expect(result).toBe(0);
+    })
+
+    test('testCase2: 人数超過なし', () => {
+        const performance = { "audience": 5 };
+        const comedy = new Comedy(plays, performance);
+        const result = comedy.point();
+        expect(result).toBe(1);
+    })
+
+    test('testCase3: 人数超過なし', () => {
+        const performance = { "audience": 6 };
+        const comedy = new Comedy(plays, performance);
+        const result = comedy.point();
+        expect(result).toBe(1);
+    })
+
+    test('testCase4: 人数超過なし', () => {
+        const performance = { "audience": 30 };
+        const comedy = new Comedy(plays, performance);
+        const result = comedy.point();
+        expect(result).toBe(6);
+    })
+
+    test('testCase5: 人数超過あり', () => {
+        const performance = { "audience": 31 };
+        const comedy = new Comedy(plays, performance);
+        const result = comedy.point();
+        expect(result).toBe(7);
+    })
+
+    test('testCase6: 人数超過あり', () => {
+        const performance = { "audience": 35 };
+        const comedy = new Comedy(plays, performance);
+        const result = comedy.point();
+        expect(result).toBe(12);
+    })
+
+    test('testCase7: 人数超過あり', () => {
+        const performance = { "audience": 36 };
+        const comedy = new Comedy(plays, performance);
+        const result = comedy.point();
+        expect(result).toBe(13);
+    })
+
+    test('testCase8: 人数０', () => {
+        const performance = { "audience": 0 };
+        const comedy = new Comedy(plays, performance);
+        const result = comedy.point();
+        expect(result).toBe(0);
+    })
+})
