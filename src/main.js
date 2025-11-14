cb0310939f788191193c764ae149a132d32c9f0a
Yumeka Ota <ota@sabira.jp>
2025-11-14 16:02:02 +0900

    関数群のクラスへの集約：thisを使用

--- src/main.js
+++ src/main.js
@@ -112,11 +112,11 @@ class CreateInvoice {
 	}
 
 	renderHtml() {
-		let invoiceHtml = `<h3>請求書</h3><h4>${createInvoice.invoice.customer}</h4><ul>`;
-		for (const performance of createInvoice.invoice.performances) {
-			invoiceHtml += `<li>${createInvoice.plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${createCalculator(createInvoice.plays, performance).amount()})</li><br>`;
+		let invoiceHtml = `<h3>請求書</h3><h4>${this.invoice.customer}</h4><ul>`;
+		for (const performance of this.invoice.performances) {
+			invoiceHtml += `<li>${this.plays[performance.playID].name} (観客数:${performance.audience}人、金額:$${createCalculator(this.plays, performance).amount()})</li><br>`;
 		}
-		const totalCalc = new TotalCalculator(createInvoice.plays, createInvoice.invoice.performances);
+		const totalCalc = new TotalCalculator(this.plays, this.invoice.performances);
 		invoiceHtml += `</ul><p>合計金額：$${totalCalc.amount()}</p>`;
 		invoiceHtml += `<p>獲得ポイント：${totalCalc.point()}pt</p>`;
 		return invoiceHtml;
