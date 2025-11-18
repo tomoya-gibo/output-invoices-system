20d1af775d04fde3020621e5e69bd4e2dc6d3797
Yumeka Ota <ota@sabira.jp>
2025-11-18 15:25:37 +0900

    関数のインライン化：元の関数をコピー

--- src/main.js
+++ src/main.js
@@ -78,6 +78,14 @@ export class TotalCalculator {
 	amount() {
 		let result = 0;
 		for (const performance of this.performances) {
+			switch (plays[performance.playID].type) {
+				case "tragedy":
+					return new Tragedy(plays, performance);
+				case "comedy":
+					return new Comedy(plays, performance);
+				default:
+					throw new Error("想定外の劇タイプです");
+			}
 			result += createCalculator(this.plays, performance).amount();
 		}
 		return result;
