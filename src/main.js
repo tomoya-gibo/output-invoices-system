a3c474e83a0133db82c7ee105b8565dc1b129c30
Yumeka Ota <ota@sabira.jp>
2025-11-18 15:30:50 +0900

    関数のインライン化：呼び出し側を修正

--- src/main.js
+++ src/main.js
@@ -89,7 +89,7 @@ export class TotalCalculator {
 				default:
 					throw new Error("想定外の劇タイプです");
 			}
-			result += createCalculator(this.plays, performance).amount();
+			result += createCalculator.amount();
 		}
 		return result;
 	}
