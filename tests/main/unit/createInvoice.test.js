fafa10d8ecde11590ce624c906f086b9bb850d73
Yumeka Ota <ota@sabira.jp>
2025-11-18 11:01:05 +0900

    テスト作成：unit/createInvoice

---
+++ tests/main/unit/createInvoice.test.js
@@ -0,0 +1,35 @@
+import { describe, test, expect } from "vitest";
+import { CreateInvoice } from "../../../src/main";
+
+const plays = { "hamlet": { "name": "Hamlet", "type": "tragedy" },
+                "as-like": { "name": "As You Like It", "type": "comedy" },
+                "othello": { "name": "Othello", "type": "tragedy" } }; 
+
+describe('CreateInvoice-renderTxtのテスト', () => {
+    test('testCase1', () => {
+        const invoice = { "customer": "Customer",
+                          "performances": [{ "playID": "hamlet", "audience": 55 },
+                                           { "playID": "as-like", "audience": 35 },
+                                           { "playID": "othello", "audience": 40 }]};
+        const createInvoice = new CreateInvoice(invoice, plays);
+        const testResult = createInvoice.renderTxt();
+        const expectData = `請求書\n\nCustomer\n\n・Hamlet (観客数:55人、金額:$65000)\n・As You Like It (観客数:35人、金額:$58000)\n`
+            + `・Othello (観客数:40人、金額:$50000)\n\n合計金額：$173000\n\n獲得ポイント：47pt\n`;
+        expect(testResult).toBe(expectData);
+    })
+})
+
+describe('CreateInvoice-renderHtmlのテスト', () => {
+    test('testCase1', () => {
+        const invoice = { "customer": "Customer",
+                          "performances": [{ "playID": "hamlet", "audience": 55 },
+                                           { "playID": "as-like", "audience": 35 },
+                                           { "playID": "othello", "audience": 40 }]};
+        const createInvoice = new CreateInvoice(invoice, plays);
+        const testResult = createInvoice.renderHtml();
+        const expectData = `<h3>請求書</h3><h4>Customer</h4><ul><li>Hamlet (観客数:55人、金額:$65000)</li><br>`
+            + `<li>As You Like It (観客数:35人、金額:$58000)</li><br><li>Othello (観客数:40人、金額:$50000)</li><br>`
+            + `</ul><p>合計金額：$173000</p><p>獲得ポイント：47pt</p>`;
+        expect(testResult).toBe(expectData);
+    })
+})
