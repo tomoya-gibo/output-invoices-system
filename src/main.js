faf7c230a698a8a91c818106e36b6ab30acb6441
kouhei <iju@sabira.jp>
2025-11-19 11:09:50 +0900

    関数の抽出 : コードを抽出先の関数にコピー

--- src/main.js
+++ src/main.js
@@ -137,6 +137,16 @@
   }
 
   function calcComedy() {
+    let thisAmount = 0;
+    const comedyBasePrice = 30000;
+    thisAmount = comedyBasePrice;
+    //超過料金の算定
+    if (performance.audience > 20) {
+      thisAmount += 10000;
+      thisAmount += (performance.audience - 20) * 500;
+    }
+    //喜劇の場合のみ超過にかかわらず一人につき$300の追加
+    thisAmount += performance.audience * 300;
   }
 
   export function calcAmount(play,performance) {
