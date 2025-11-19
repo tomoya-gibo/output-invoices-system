cc16a03edfd20df44412ac1fe40f3c43013ee092
kouhei <iju@sabira.jp>
2025-11-19 11:34:28 +0900

    ポリモーフィズムによる条件記述の置き換え : サブクラスのメソッドの作成

--- src/main.js
+++ src/main.js
@@ -134,6 +134,17 @@
       super(play,performance);
     }
     amount(){
+      let thisAmount = 0;
+      const comedyBasePrice = 30000;
+      thisAmount = comedyBasePrice;
+      //超過料金の算定
+      if (performance.audience > 20) {
+        thisAmount += 10000;
+        thisAmount += (performance.audience - 20) * 500;
+      }
+      //喜劇の場合のみ超過にかかわらず一人につき$300の追加
+      thisAmount += performance.audience * 300;
+      return thisAmount;
     }
   }
 
