6df8fa0896a207712891a848050ac175fc65525f
Yumeka Ota <ota@sabira.jp>
2025-11-20 14:39:46 +0900

    関数宣言の変更：新たな関数の作成

--- src/main.js
+++ src/main.js
@@ -98,6 +98,9 @@ export class CreateInvoice {
 	constructor(invoice, plays, totalCalc) {
 		this._data = { invoice: invoice, plays: plays, totalCalc: totalCalc };
 	}
+	zz_constructor(invoice, plays, totalCalc) {
+		
+	}
 	get invoice() { return this._data.invoice; }
 	get plays() { return this._data.plays; }
 	get totalCalc() { return this._data.totalCalc; }
