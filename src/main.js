128655444b9033efe80b5c5ff91d4d91091f27fb
Yumeka Ota <ota@sabira.jp>
2025-11-20 13:19:37 +0900

    関数宣言の変更：元の関数の呼び出し部分を置き換え

--- src/main.js
+++ src/main.js
@@ -147,7 +147,7 @@ export function main() {
 	const arg = process.argv.slice(2)[0];			// txt/html
 
 	const totalCalc = new TotalCalculator(plays, invoices[0].performances);
-	zz_printInvoice(invoices[0], plays, totalCalc, "txt");	// テスト用にargを指定
+	printInvoice(invoices[0], plays, totalCalc, "txt");	// テスト用にargを指定
 }
 
 // main();
