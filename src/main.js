bcd4c82bb091ba546d7ad470fb9fb06ff0e1d46c
Yumeka Ota <ota@sabira.jp>
2025-11-20 14:55:40 +0900

    関数宣言の変更：呼び出し部分のパラメータの削除

--- src/main.js
+++ src/main.js
@@ -144,7 +144,7 @@ export function main() {
 	const arg = process.argv.slice(2)[0];			// txt/html
 
 	const totalCalc = new TotalCalculator(plays, invoices[0].performances);
-	const createInvoice = new CreateInvoice(invoices[0], plays, totalCalc);
+	const createInvoice = new CreateInvoice(invoices[0], totalCalc);
 	printInvoice(createInvoice, "txt");	// テスト用にargを指定
 }
 
