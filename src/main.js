6b966563a29db495e7640c1baa6b19fc25c8f7f1
Yumeka Ota <ota@sabira.jp>
2025-11-20 13:13:15 +0900

    パラメータの追加(関数のインライン化)：呼び出し側を変更

--- src/main.js
+++ src/main.js
@@ -148,7 +148,7 @@ export function main() {
 	const arg = process.argv.slice(2)[0];			// txt/html
 
 	const totalCalc = new TotalCalculator(plays, invoices[0].performances);
-	printInvoice(invoices[0], plays, "txt");	// テスト用にargを指定
+	zz_printInvoice(invoices[0], plays, totalCalc, "txt");	// テスト用にargを指定
 }
 
 // main();
