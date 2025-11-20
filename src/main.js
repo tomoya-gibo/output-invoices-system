7279af269110b6d7f8c29cf905b58d11316833b7
kouhei <iju@sabira.jp>
2025-11-20 11:20:58 +0900

    関数の抽出 : playFor関数にパラメータとリターン文を追加

--- src/main.js
+++ src/main.js
@@ -55,13 +55,8 @@
     }
   }
 
-  function playFor() {
-    for (let performance of invoices[0].performances) {
-      // playsのキーとperformance.playIDを照合してplayに代入
-      const play = plays[performance.playID];
-      //合計金額
-      totalAmount += calcAmount(play,performance);
-    }
+  function playFor(plays,performance) {
+    return plays[performance.playID];
   }
 
   export function calculateTotalAmounts(invoices,plays) {
